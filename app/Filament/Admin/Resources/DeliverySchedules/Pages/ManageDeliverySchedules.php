8c64985ce78d591cc34d558b4c15be1238791498
yusufbayuw <yusufbayu31@gmail.com>
2026-09-13 16:45:12 +0700

    feat: add delivery schedule management page

---
+++ app/Filament/Admin/Resources/DeliverySchedules/Pages/ManageDeliverySchedules.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Admin\Resources\DeliverySchedules\Pages;
+
+use App\Filament\Admin\Resources\DeliverySchedules\DeliveryScheduleResource;
+use Filament\Resources\Pages\ManageRecords;
+
+class ManageDeliverySchedules extends ManageRecords
+{
+    protected static string $resource = DeliveryScheduleResource::class;
+}
