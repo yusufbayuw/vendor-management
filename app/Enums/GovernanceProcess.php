8c99a0c49ee6bbed04182495e750bf88664c1a85
yusufbayuw <yusufbayu31@gmail.com>
2026-09-13 18:30:43 +0700

    feat: label configurable governance processes

--- app/Enums/GovernanceProcess.php
+++ app/Enums/GovernanceProcess.php
@@ -11,4 +11,17 @@ enum GovernanceProcess: string
     case SupplierBankAccountChange = 'supplier.bank_account_change';
     case InvoiceApproval = 'invoice.approval';
     case PaymentVerification = 'payment.verification';
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::PurchaseRequestApproval => 'Approval Purchase Request',
+            self::PurchaseOrderApproval => 'Approval Purchase Order',
+            self::PurchaseOrderExceptionClosing => 'Penutupan PO dengan Exception',
+            self::SupplierVerification => 'Verifikasi Supplier',
+            self::SupplierBankAccountChange => 'Perubahan Rekening Supplier',
+            self::InvoiceApproval => 'Approval Invoice',
+            self::PaymentVerification => 'Verifikasi Pembayaran',
+        };
+    }
 }
