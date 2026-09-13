274e0c928aff709314d1f64114df1eacfb4a74c8
yusufbayuw <yusufbayu31@gmail.com>
2026-09-13 17:03:05 +0700

    feat: add admin supplier documents page

---
+++ app/Filament/Admin/Resources/SupplierDocuments/Pages/ManageSupplierDocuments.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Admin\Resources\SupplierDocuments\Pages;
+
+use App\Filament\Admin\Resources\SupplierDocuments\SupplierDocumentResource;
+use Filament\Resources\Pages\ManageRecords;
+
+class ManageSupplierDocuments extends ManageRecords
+{
+    protected static string $resource = SupplierDocumentResource::class;
+}
