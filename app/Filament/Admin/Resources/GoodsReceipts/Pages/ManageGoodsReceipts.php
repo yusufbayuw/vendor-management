468dbe73e116519cb71531c3d9dd4a134633a842
yusufbayuw <yusufbayu31@gmail.com>
2026-09-13 16:46:03 +0700

    feat: add goods receipt management page

---
+++ app/Filament/Admin/Resources/GoodsReceipts/Pages/ManageGoodsReceipts.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Admin\Resources\GoodsReceipts\Pages;
+
+use App\Filament\Admin\Resources\GoodsReceipts\GoodsReceiptResource;
+use Filament\Resources\Pages\ManageRecords;
+
+class ManageGoodsReceipts extends ManageRecords
+{
+    protected static string $resource = GoodsReceiptResource::class;
+}
