1c80f4df579aa4cdbff172bd3e9e71a2a3cd7b2b
yusufbayuw <yusufbayu31@gmail.com>
2026-09-13 16:58:28 +0700

    feat: add supplier profile page

---
+++ app/Filament/Supplier/Resources/Profiles/Pages/ManageSupplierProfiles.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Supplier\Resources\Profiles\Pages;
+
+use App\Filament\Supplier\Resources\Profiles\SupplierProfileResource;
+use Filament\Resources\Pages\ManageRecords;
+
+class ManageSupplierProfiles extends ManageRecords
+{
+    protected static string $resource = SupplierProfileResource::class;
+}
