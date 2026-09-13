e4bcdd2a9192c514908c83b7047923e3a9c926ad
yusufbayuw <yusufbayu31@gmail.com>
2026-09-14 05:36:25 +0700

    feat: configure web push vapid settings

---
+++ config/webpush.php
@@ -0,0 +1,18 @@
+<?php
+
+use NotificationChannels\WebPush\PushSubscription;
+
+return [
+    'vapid' => [
+        'subject' => env('VAPID_SUBJECT') ?: 'mailto:'.env('MAIL_FROM_ADDRESS', 'noreply@example.com'),
+        'public_key' => env('VAPID_PUBLIC_KEY'),
+        'private_key' => env('VAPID_PRIVATE_KEY'),
+        'pem_file' => env('VAPID_PEM_FILE'),
+    ],
+
+    'model' => PushSubscription::class,
+    'table_name' => env('WEBPUSH_DB_TABLE', 'push_subscriptions'),
+    'database_connection' => env('WEBPUSH_DB_CONNECTION', env('DB_CONNECTION')),
+    'client_options' => [],
+    'automatic_padding' => env('WEBPUSH_AUTOMATIC_PADDING', true),
+];
