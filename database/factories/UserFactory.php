a34265d57a452fd5212b5dbd8841d10cf27e707d
yusufbayuw <yusufbayu31@gmail.com>
2026-09-13 17:11:46 +0700

    test: include username in user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,7 +26,9 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'username' => Str::lower(fake()->unique()->userName()),
             'email' => fake()->unique()->safeEmail(),
+            'phone' => null,
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
