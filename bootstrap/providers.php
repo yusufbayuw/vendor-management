c8c01748209145ec7b621d2484b49b943e3afde7
yusufbayuw <yusufbayu31@gmail.com>
2026-09-13 16:17:45 +0700

    feat: register internal and supplier panels

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Providers\AppServiceProvider;
+use App\Providers\Filament\AdminPanelProvider;
+use App\Providers\Filament\SupplierPanelProvider;
 
 return [
     AppServiceProvider::class,
+    AdminPanelProvider::class,
+    SupplierPanelProvider::class,
 ];
