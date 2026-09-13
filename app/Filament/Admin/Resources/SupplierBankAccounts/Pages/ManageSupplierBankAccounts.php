476443e2f97bbd2eef754d53fe624b45638e2aa7
yusufbayuw <yusufbayu31@gmail.com>
2026-09-13 17:03:30 +0700

    feat: add admin supplier bank accounts page

---
+++ app/Filament/Admin/Resources/SupplierBankAccounts/Pages/ManageSupplierBankAccounts.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Admin\Resources\SupplierBankAccounts\Pages;
+
+use App\Filament\Admin\Resources\SupplierBankAccounts\SupplierBankAccountResource;
+use Filament\Resources\Pages\ManageRecords;
+
+class ManageSupplierBankAccounts extends ManageRecords
+{
+    protected static string $resource = SupplierBankAccountResource::class;
+}
