154ade8560630b7ff1b3171dc9df68724e76254b
yusufbayuw <yusufbayu31@gmail.com>
2026-09-13 16:53:05 +0700

    feat: add supplier delivery page

---
+++ app/Filament/Supplier/Resources/DeliverySchedules/Pages/ManageDeliverySchedules.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Supplier\Resources\DeliverySchedules\Pages;
+
+use App\Filament\Supplier\Resources\DeliverySchedules\DeliveryScheduleResource;
+use Filament\Resources\Pages\ManageRecords;
+
+class ManageDeliverySchedules extends ManageRecords
+{
+    protected static string $resource = DeliveryScheduleResource::class;
+}
