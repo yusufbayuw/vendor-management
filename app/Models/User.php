f136b4bdac8ed929595e055cae004f6f7a9b6f0f
yusufbayuw <yusufbayu31@gmail.com>
2026-09-13 16:17:06 +0700

    feat: authorize Filament panels by business role

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,24 +2,59 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Enums\SystemRole;
 use Database\Factories\UserFactory;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, HasRoles, Notifiable;
+
+    public function accessScopes(): HasMany
+    {
+        return $this->hasMany(UserAccessScope::class);
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        if ($this->hasRole(SystemRole::SuperAdmin->value)) {
+            return true;
+        }
+
+        return match ($panel->getId()) {
+            'supplier' => $this->hasAnyRole([
+                SystemRole::SupplierAdmin->value,
+                SystemRole::SupplierOperator->value,
+            ]),
+            'admin' => $this->hasAnyRole([
+                SystemRole::PanelUser->value,
+                SystemRole::CentralManager->value,
+                SystemRole::SppgManager->value,
+                SystemRole::Requester->value,
+                SystemRole::Procurement->value,
+                SystemRole::ProcurementManager->value,
+                SystemRole::Receiver->value,
+                SystemRole::QualityControl->value,
+                SystemRole::Finance->value,
+                SystemRole::FinanceManager->value,
+                SystemRole::Auditor->value,
+            ]),
+            default => false,
+        };
+    }
 
     /**
-     * Get the attributes that should be cast.
-     *
      * @return array<string, string>
      */
     protected function casts(): array
