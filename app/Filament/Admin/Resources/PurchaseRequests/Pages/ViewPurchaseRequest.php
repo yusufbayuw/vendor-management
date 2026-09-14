bc79b1e8cfcad634cf2486fdca40a840413fa5cc
yusufbayuw <yusufbayu31@gmail.com>
2026-09-14 11:40:28 +0700

    feat: add purchase request view page

---
+++ app/Filament/Admin/Resources/PurchaseRequests/Pages/ViewPurchaseRequest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Admin\Resources\PurchaseRequests\Pages;
+
+use App\Filament\Admin\Resources\PurchaseRequests\PurchaseRequestResource;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewPurchaseRequest extends ViewRecord
+{
+    protected static string $resource = PurchaseRequestResource::class;
+}
