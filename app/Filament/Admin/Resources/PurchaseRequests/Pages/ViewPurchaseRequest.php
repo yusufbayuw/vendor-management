d75de41b2f9a17d468f81f61c42f89ffe6e2a990
yusufbayuw <yusufbayu31@gmail.com>
2026-09-14 11:49:38 +0700

    ux: attach allocation workspace to PR detail

--- app/Filament/Admin/Resources/PurchaseRequests/Pages/ViewPurchaseRequest.php
+++ app/Filament/Admin/Resources/PurchaseRequests/Pages/ViewPurchaseRequest.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Admin\Resources\PurchaseRequests\Pages;
 
 use App\Filament\Admin\Resources\PurchaseRequests\PurchaseRequestResource;
+use App\Filament\Admin\Resources\PurchaseRequests\RelationManagers\ItemsRelationManager;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewPurchaseRequest extends ViewRecord
 {
     protected static string $resource = PurchaseRequestResource::class;
+
+    protected function getAllRelationManagers(): array
+    {
+        return [
+            ItemsRelationManager::class,
+        ];
+    }
 }
