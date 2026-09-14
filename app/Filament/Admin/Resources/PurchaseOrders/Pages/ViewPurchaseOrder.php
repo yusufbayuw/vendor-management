f8caa9896732599328ad9909263dd87ecdec3262
yusufbayuw <yusufbayu31@gmail.com>
2026-09-14 11:44:19 +0700

    feat: add purchase order view page

---
+++ app/Filament/Admin/Resources/PurchaseOrders/Pages/ViewPurchaseOrder.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Admin\Resources\PurchaseOrders\Pages;
+
+use App\Filament\Admin\Resources\PurchaseOrders\PurchaseOrderResource;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewPurchaseOrder extends ViewRecord
+{
+    protected static string $resource = PurchaseOrderResource::class;
+}
