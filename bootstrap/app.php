d2cb947696a6916690dac01b895e39b6c39821a1
yusufbayuw <yusufbayu31@gmail.com>
2026-09-26 13:35:20 +0700

    fix: correct license middleware class references

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->web(append: [
-            \\App\\Http\\Middleware\\EnsureLicenseIsValid::class,
+            \App\Http\Middleware\EnsureLicenseIsValid::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
